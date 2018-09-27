9eb5dabf321a63225b458956952f8005e4e221bf
huyuhui001 <james.hu01@sap.com>
2018-09-27 23:11:39 +0800

    2018/9/27 Rev2

--- src/pkg/intermediate/jframe/MainJFrame.java
+++ src/pkg/intermediate/jframe/MainJFrame.java
@@ -1,28 +1,24 @@
 package pkg.intermediate.jframe;
 
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Paint;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 class MainJFrame extends JFrame {
+	private static final long serialVersionUID = -6493028989486177335L;
+
 	public static void main(String[] args) throws InterruptedException {
 		MainJFrame newWindow = new MainJFrame();
 		Draw newBall = new Draw();
-		Keyboard newKeyboard = new Keyboard();
 		
 		while(true) {
-//			newWindow.add(newBall);
-//			newBall.moveBall();
-			newWindow.add(newKeyboard);
+			newWindow.add(newBall);
+			newBall.moveBall();
 			newWindow.repaint();
-			Thread.sleep(2);
+			Thread.sleep(5);
 		}
 	}
 	
@@ -35,6 +31,7 @@ class MainJFrame extends JFrame {
 }
 
 class Draw extends JPanel {
+	private static final long serialVersionUID = -8055648903511266979L;
 	int x = 0;
 	int y = 0;
 	int x_inc = 0;
@@ -73,39 +70,39 @@ class Draw extends JPanel {
 
 
 
-class Keyboard extends JPanel {
-	public Keyboard() {
-		KeyListener myListener = new MyKeyListener(this);
-		addKeyListener(myListener);
-		setFocusable(true);
-	}
-}
-
-
-
-
-class MyKeyListener implements KeyListener {
-	String str1 = "";
-	private Keyboard myKB;
-		
-	MyKeyListener (Keyboard myKB) {
-		this.myKB = myKB;
-	}
-	
-	@Override
-	public void keyTyped(KeyEvent e) {		
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		str1 = KeyEvent.getKeyText(e.getKeyCode());
-//		myKB.repaint();
-		System.out.println("Pressed Key is: "+KeyEvent.getKeyText(e.getKeyCode()));
-	}
- 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		System.out.println("Released Key is:  " + KeyEvent.getKeyText(e.getKeyCode()));	
-	}
-}
+//class Keyboard extends JPanel {
+//	public Keyboard() {
+//		KeyListener myListener = new MyKeyListener(this);
+//		addKeyListener(myListener);
+//		setFocusable(true);
+//	}
+//}
+//
+//
+//
+//
+//class MyKeyListener implements KeyListener {
+//	String str1 = "";
+//	private Keyboard myKB;
+//		
+//	MyKeyListener (Keyboard myKB) {
+//		this.myKB = myKB;
+//	}
+//	
+//	@Override
+//	public void keyTyped(KeyEvent e) {		
+//	}
+//
+//	@Override
+//	public void keyPressed(KeyEvent e) {
+//		str1 = KeyEvent.getKeyText(e.getKeyCode());
+////		myKB.repaint();
+//		System.out.println("Pressed Key is: "+KeyEvent.getKeyText(e.getKeyCode()));
+//	}
+// 
+//	@Override
+//	public void keyReleased(KeyEvent e) {
+//		System.out.println("Released Key is:  " + KeyEvent.getKeyText(e.getKeyCode()));	
+//	}
+//}
 
